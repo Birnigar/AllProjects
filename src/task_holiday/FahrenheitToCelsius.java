4210b2452479c090755943ef6efea6b77f956f1c
Birnigar <farukgalatasaray34@gmail.com>
2019-12-26 04:59:38 -0800

    B15 Cybertek

---
+++ src/task_holiday/FahrenheitToCelsius.java
@@ -0,0 +1,17 @@
+package task_holiday;
+
+public class FahrenheitToCelsius {
+    public static void main(String[] args) {
+
+        System.out.println(fahrenheitToCelsius(45.0)+" C.");
+
+
+
+    }
+
+    public static double  fahrenheitToCelsius(double fahrenheit){
+double celsius;
+     return    celsius = (5.0 / 9) * (fahrenheit - 32);
+
+    }
+}
