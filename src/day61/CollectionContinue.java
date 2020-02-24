63f302c2adfb9820844ffe327d77685e87a3a89a
Birnigar <farukgalatasaray34@gmail.com>
2020-02-23 22:57:49 -0800

    B15 Cybertek

---
+++ src/day61/CollectionContinue.java
@@ -0,0 +1,30 @@
+package day61;
+
+import java.util.*;
+
+public class CollectionContinue {
+    public static void main(String[] args) {
+
+        Collection<Integer> nums=new ArrayList<>();
+        
+        nums.addAll(Arrays.asList(1,6,4,5,7,8));
+        System.out.println("nums = " + nums);
+        List<Integer> nums3=(List<Integer>) nums; //down casting
+        Collections.sort(nums3);
+        System.out.println("nums3 = " + nums3);
+
+        //can we get second Item  big nooooo!!because collection interface has not have get method.
+
+        List<Integer> nums2=new ArrayList<>(Arrays.asList(10,7,4,11,67,8));
+        
+        Collections.sort(nums2);
+
+        System.out.println("nums2 = " + nums2);
+        
+        
+        
+        
+        
+        
+    }
+}
---
+++ src/day61/List_LinkedListImplementation.java
@@ -0,0 +1,33 @@
+package day61;
+
+import java.util.*;
+
+public class List_LinkedListImplementation {
+    public static void main(String[] args) {
+
+       // List<String> lst=new ArrayList<>();
+        List<String> lst=new LinkedList<>();
+        
+        lst.add("Zeynep");
+        lst.add("Ershat");
+        lst.add("Kamira");
+        lst.add("Sabira");
+
+        System.out.println("lst = " + lst);
+        lst.remove("Ershat");
+        System.out.println("lst = " + lst);
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
